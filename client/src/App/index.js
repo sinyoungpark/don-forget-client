87c3e4f49024b3895cb7bc87f74ac5dc811392d5
sinyoung98 <joyhope3@naver.com>
2022-12-14 00:20:11 +0900

    edit App component
    로그인 인증
    1. user에 window.sessionStorage.getItem("id") 값 담기
    2. UserContext 로 전역변수로 사용

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -5,28 +5,34 @@ import { useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import Nav from "../component/Nav";
 import "./App.scss";
-import { AppContainer } from "./styles";
 
 export const UserContext = createContext([]);
 
 export default function App() {
   const location = useLocation();
-  const [user, setUser] = useState(window.sessionStorage.getItem("id"));
+  const [user, setUser] = useState({
+    id : window.sessionStorage.getItem("id"),
+    email :  window.sessionStorage.getItem("email"),
+    name : window.sessionStorage.getItem("name")
+  });
 
   return (
     <div className="app">
-      <Nav />
-      <div
-        className={
-          location.pathname === "/intro"
-            ? "background"
-            : location.pathname === "/signin" || location.pathname === "/signup"
-            ? "fullBackground"
-            : "sideBackground"
-        }
-      >
-        <Outlet />
-      </div>
+      <UserContext.Provider value={[user, setUser]}>
+        <Nav />
+        <div
+          className={
+            location.pathname === "/intro"
+              ? "background"
+              : location.pathname === "/signin" ||
+                location.pathname === "/signup"
+              ? "fullBackground"
+              : "sideBackground"
+          }
+        >
+          <Outlet />
+        </div>
+      </UserContext.Provider>
     </div>
   );
 }
